4bbaacbfaf8c18f2395555fb6087f3e428f866ea
mbukerepo <mbuke404@gmail.com>
2022-10-31 19:05:10 +0200

    finished responsiveness

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,3 +1,14 @@
-const Button = () => {
-    return ();
-} 
+const Button = (props: any) => {
+  let classes =
+    "text-white outline-none cursor-pointer text-sm md:text-base rounded-full bg-zinc-700 px-5 py-2";
+  if (props.variant === "primary") {
+    classes =
+      "border-zinc-700 border outline-none cursor-pointer text-sm rounded-full bg-white text-zinc-700 px-5 py-2";
+  }
+  if (props.variant === "secondary") {
+    classes =
+      "border-zinc-300 border outline-none cursor-pointer text-sm rounded-full bg-white font-secondary text-zinc-600 px-5 py-2";
+  }
+  return <button className={classes}>{props.children}</button>;
+};
+export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
+import Button from "../components/UI/Button";
 export default function Home() {
   const toLearningClick = () => {
     document.querySelector("#learning")?.scrollIntoView({ behavior: "smooth" });
@@ -22,7 +23,7 @@ export default function Home() {
         <div className="relative max-w-6xl w-full flex mx-auto">
           <div className="py-6 bg-white w-full   px-2 sm:px-4 md:px-12 lg:px-[7rem] flex justify-between">
             <div className="flex  items-center">
-              <div className="flex items-center gap-1 border-r border-gray-300 mx-[0.30rem]  px-[0.30rem]">
+              <div className="flex items-center gap-1 border-r border-gray-300 mx-[0.6rem]  px-[0.6rem]">
                 <div className="w-[2.4375rem]  px-[0.30rem]">
                   <Image
                     src="/logos/logo-mobile.svg"
@@ -57,9 +58,7 @@ export default function Home() {
                 Start The Course
               </Link>
               <Link href={"#"}>
-                <button className="text-white outline-none cursor-pointer text-sm md:text-base rounded-full bg-zinc-700 px-5 py-2">
-                  Create wallet
-                </button>
+                <Button>Create wallet</Button>
               </Link>
             </div>
           </div>
@@ -100,14 +99,14 @@ export default function Home() {
       </div>
       <div
         id="learning"
-        className="min-h-screen pt-24  flex flex-col gap-24 items-center"
+        className="min-h-screen pt-24  flex flex-col  items-center"
       >
         <h2 className="text-zinc-700 text-center md:text-5xl font-secondary text-3xl">
           Making your NEAR
           <br /> learning easy.
         </h2>
         <div className="max-w-4xl relative">
-          <div className="w-64 h-64 absolute -top-20 -right-10 open-wallet-gradient"></div>
+          <div className="w-64 h-64 absolute hidden md:block -top-20 -right-10 open-wallet-gradient"></div>
           <div className="flex flex-col md:flex-row-reverse gap-4 items-center px-[1.375rem]">
             <div className="bg-white relative z-10 lg:w-[25rem]">
               <Image
@@ -127,81 +126,102 @@ export default function Home() {
                 <span> as well as starting this course.</span>
               </p>
               <Link href={"#"}>
-                <button className="border-zinc-700 border outline-none cursor-pointer text-sm rounded-full bg-white text-zinc-700 px-5 py-2">
-                  Create wallet
-                </button>
+                <Button variant="primary">Create wallet</Button>
               </Link>
             </div>
           </div>
         </div>
-
-        <div className="flex flex-col gap-4 items-center px-[1.375rem]">
-          <div>
-            <Image
-              alt="complete course ..."
-              src="/images/course.svg"
-              width={180}
-              height={180}
-            />
-          </div>
-          <div className="flex flex-col gap-4 items-center">
-            <h4 className="font-secondary text-zinc-700  text-xl">
-              Complete the course
-            </h4>
-            <p className="paragraph-text-color">
-              In three simple modules, learn:
-            </p>
-            <div className="flex flex-col gap-5 items-center">
-              <div className="flex flex-col items-center gap-3">
-                <div className="w-5 h-5">
-                  <Image
-                    src="/images/course-1.png"
-                    width={128}
-                    height={128}
-                    alt="course 1..."
-                  />
+        <div className="hidden md:block">
+          <Image
+            src="/images/arrow1.svg"
+            height={163}
+            width={132}
+            alt="arrow ..."
+          />
+        </div>
+        <div className="max-w-4xl relative">
+          <div className="flex flex-col md:flex-row items-center md:w-[58rem] gap-3 md:gap-[14rem]  px-[1.375rem]">
+            <div className="w-[14rem]">
+              <Image
+                alt="complete course ..."
+                src="/images/course.svg"
+                width={180}
+                height={180}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-4 items-center md:items-start">
+              <h4 className="font-secondary md:text-3xl text-zinc-700  text-xl">
+                Complete the course
+              </h4>
+              <p className="paragraph-text-color">
+                In three simple modules, learn:
+              </p>
+              <div className="flex flex-col gap-5 items-center md:items-start">
+                <div className="flex flex-col md:flex-row items-center gap-3">
+                  <div className="w-5 h-5">
+                    <Image
+                      src="/images/course-1.png"
+                      width={128}
+                      height={128}
+                      alt="course 1..."
+                    />
+                  </div>
+                  <p className="paragraph-text-color text-sm">
+                    Essential NEAR concepts
+                  </p>
                 </div>
-                <p className="paragraph-text-color text-sm">
-                  Essential NEAR concepts
-                </p>
-              </div>
-              <div className="flex flex-col items-center gap-3">
-                <div className="w-5 h-5">
-                  <Image
-                    src="/images/course-2.png"
-                    width={128}
-                    height={128}
-                    alt="course 1..."
-                  />
+                <div className="flex flex-col md:flex-row items-center gap-3">
+                  <div className="w-5 h-5">
+                    <Image
+                      src="/images/course-2.png"
+                      width={128}
+                      height={128}
+                      alt="course 1..."
+                    />
+                  </div>
+                  <p className="paragraph-text-color text-sm">
+                    How to write smart contracts on NEAR
+                  </p>
                 </div>
-                <p className="paragraph-text-color text-sm">
-                  How to write smart contracts on NEAR
-                </p>
-              </div>
-              <div className="flex flex-col items-center gap-3">
-                <div className="w-5 h-5">
-                  <Image
-                    src="/images/course-3.png"
-                    width={128}
-                    height={128}
-                    alt="course 1..."
-                  />
+                <div className="flex flex-col md:flex-row items-center gap-3">
+                  <div className="w-5 h-5">
+                    <Image
+                      src="/images/course-3.png"
+                      width={128}
+                      height={128}
+                      alt="course 1..."
+                    />
+                  </div>
+                  <p className="paragraph-text-color text-sm">
+                    How to build dapps on the NEAR blockchain
+                  </p>
                 </div>
-                <p className="paragraph-text-color text-sm">
-                  How to build dapps on the NEAR blockchain
-                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-4 items-center px-[1.375rem]">
-          <h4 className="font-secondary text-zinc-700  text-xl">
-            Claim NEAR certificate
-          </h4>
-          <p className="paragraph-text-color">
-            Upon course completion, receive the NEAR dev 101 certificate as an
-            NFT on your NEAR wallet.
-          </p>
+        <div className="hidden md:block">
+          <Image
+            src="/images/arrow2.svg"
+            alt="arrow 2"
+            width={163}
+            height={129}
+          />
+        </div>
+        <div className="flex flex-col gap-16 items-center px-[1.375rem]">
+          <div className="flex flex-col gap-4 items-center">
+            <h4
+              className="font-secondary mt-12
+             text-zinc-700 md:text-3xl  text-xl"
+            >
+              Claim NEAR certificate
+            </h4>
+            <p className="paragraph-text-color w-3/5 text-center">
+              Upon course completion, receive the NEAR dev 101 certificate as an
+              NFT on your NEAR wallet.
+            </p>
+          </div>
           <div>
             <Image
               src="/images/certificate.png"
@@ -211,93 +231,107 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex flex-col gap-4 items-center px-[1.375rem]">
-          <div className="w-[11.25rem]">
-            <Image
-              src="/images/near-grey.png"
-              alt="Near blockchain logo"
-              width={400}
-              height={400}
-            />
-          </div>
-          <div className="flex flex-col items-center gap-4">
-            <h4 className="font-secondary text-zinc-700  text-xl">
-              NEAR Development 101
-            </h4>
-            <Link href={"#"}>
-              <button className="border-zinc-300 border outline-none cursor-pointer text-sm rounded-full bg-white font-secondary text-zinc-600 px-5 py-2">
-                Start The Course
-              </button>
-            </Link>
+        <div className="min-w-[30rem]  overflow-hidden relative">
+          <div className="near-development-gradient w-[60rem] absolute bottom-0 hidden md:block right-0 -z-10  h-32"></div>
+          <div className="flex flex-col pt-36 md:gap-[14rem] gap-4 md:flex-row-reverse items-center px-[1.375rem]">
+            <div className="md:w-[23rem]">
+              <Image
+                src="/images/near-grey.png"
+                alt="Near blockchain logo"
+                width={180}
+                height={180}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col md:items-start items-center gap-4">
+              <h4 className="font-secondary text-zinc-700  text-xl">
+                NEAR Development 101
+              </h4>
+              <Link href={"#"}>
+                <Button variant="secondary">Start The Course</Button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="py-24 flex flex-col gap-24 items-center text-center text-zinc-700 px-6">
+      <section className="py-24 flex flex-col gap-12 items-center text-center text-zinc-700 px-6">
         <p>
           Visit{" "}
           <span className="font-tartiary text-zinc-700">nearbalkans.org</span>{" "}
           and make sure to follow us on our social and community channels!
         </p>
-        <div className="flex flex-col gap-8">
-          <div className="grid grid-cols-7 gap-8 items-center justify-center w-full">
-            <div>
+        <div className="flex flex-col gap-12 items-center">
+          <div className="flex flex-row gap-16 items-center  max-w-2xl md:justify-center md:mx-auto">
+            <div className="hidden w-64 md:block md:border-r-2 pr-16  md:border-gray-300 ">
               <Image
-                src="/icons/twitter.svg"
-                width={29}
-                height={24}
-                alt="twitter icon"
+                src="/logos/near-desktop.svg"
+                width={119}
+                height={38}
+                alt="near desktop"
+                className="w-full"
               />
             </div>
-            <div>
-              <Image
-                width={28}
-                height={22}
-                src="/icons/discord.svg"
-                alt="discord icon"
-              />
-            </div>
-            <div>
-              <Image
-                width={28}
-                height={22}
-                src="/icons/telegram.svg"
-                alt="telegram icon"
-              />
-            </div>
-            <div>
-              <Image
-                width={15}
-                height={28}
-                src="/icons/facebook.svg"
-                alt="facebook icon"
-              />
-            </div>
-            <div>
-              <Image
-                width={25}
-                height={24}
-                src="/icons/linkedin.svg"
-                alt="linked IN icon"
-              />
-            </div>
-            <div>
-              <Image
-                width={28}
-                height={28}
-                src="/icons/instagram.svg"
-                alt="instagram icon"
-              />
-            </div>
-            <div>
-              <Image
-                width={32}
-                height={18}
-                src="/icons/medium.svg"
-                alt="instagram icon"
-              />
+            <div className="grid grid-cols-7 gap-8 md:gap-1 items-center justify-center w-full">
+              <div>
+                <Image
+                  src="/icons/twitter.svg"
+                  width={29}
+                  height={24}
+                  alt="twitter icon"
+                />
+              </div>
+              <div>
+                <Image
+                  width={28}
+                  height={22}
+                  src="/icons/discord.svg"
+                  alt="discord icon"
+                />
+              </div>
+              <div>
+                <Image
+                  width={28}
+                  height={22}
+                  src="/icons/telegram.svg"
+                  alt="telegram icon"
+                />
+              </div>
+              <div>
+                <Image
+                  width={15}
+                  height={28}
+                  src="/icons/facebook.svg"
+                  alt="facebook icon"
+                />
+              </div>
+              <div>
+                <Image
+                  width={25}
+                  height={24}
+                  src="/icons/linkedin.svg"
+                  alt="linked IN icon"
+                />
+              </div>
+              <div>
+                <Image
+                  width={28}
+                  height={28}
+                  src="/icons/instagram.svg"
+                  alt="instagram icon"
+                />
+              </div>
+              <div>
+                <Image
+                  width={32}
+                  height={18}
+                  src="/icons/medium.svg"
+                  alt="instagram icon"
+                />
+              </div>
             </div>
           </div>
-          <ul>
+
+          <ul className="flex flex-col md:flex-row gap-4">
             <li>NEAR Balkans 2022</li>
             <li>info@nearbalkans.org</li>
             <li>Privacy Policy</li>
@@ -306,10 +340,11 @@ export default function Home() {
           </ul>
           <p>
             NEAR Balkans Hub: Bosnia & Herzegovina, Bulgaria, Croatia,
-            Montenegro, North Macedonia, Serbia, and Slovenia
+            Montenegro, North Macedonia, Serbia, and
+            <br /> Slovenia
           </p>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
